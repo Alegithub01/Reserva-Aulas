791251ac3ab125371dc0dd7b8f184e2e04a72d7d
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-20 23:46:03 -0400

    Superposicion de la barra lateral

--- frontend/src/Navegacion/BarraLateral.jsx
+++ frontend/src/Navegacion/BarraLateral.jsx
@@ -1,49 +1,25 @@
-import { useState } from 'react';
-import { IconButton, Toolbar, Drawer, List, ListItem, ListItemIcon, ListItemText, Divider } from '@mui/material';
-// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-// import MailIcon from '@mui/icons-material/Mail';
+// Nuevo componente para la barra lateral
+import { Drawer, List, ListItem, ListItemText } from '@mui/material';
+import PropTypes from 'prop-types';
 
-
-const drawerWidth = 240;
-
-const BarraLateral = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
+const BarraLateral = ({ isOpen, onClose }) => {
   return (
-    <Drawer
-      variant="permanent"
-      open={open}
-      sx={{
-        width: open ? drawerWidth : 0,
-        flexShrink: 0,
-        [`& .MuiDrawer-paper`]: { width: open ? drawerWidth : 0, boxSizing: 'border-box' },
-      }}
-    >
-      {/* <Toolbar>
-        <IconButton onClick={open ? handleDrawerClose : handleDrawerOpen}>
-          {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-        </IconButton>
-      </Toolbar> */}
-      <Divider />
+    <Drawer anchor="left" open={isOpen} onClose={onClose}>
       <List>
         <ListItem button>
-          <ListItemIcon>
-            {/* <MailIcon /> */}
-          </ListItemIcon>
-          <ListItemText primary="Correo" />
+          <ListItemText primary="a" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="b" />
         </ListItem>
       </List>
     </Drawer>
   );
+};
+
+BarraLateral.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired
 };
 
 export default BarraLateral;
--- frontend/src/Navegacion/BarraSuperior.jsx
+++ frontend/src/Navegacion/BarraSuperior.jsx
@@ -1,16 +1,22 @@
 import { AppBar, Toolbar, IconButton, Typography, Tooltip, Box } from '@mui/material';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu';
 import { useTheme } from '../Contexts/ThemeContext';
+import PropTypes from 'prop-types';
 
-const BarraSuperior = () => {
+const BarraSuperior = ({ onToggleSidebar }) => {
   const { theme } = useTheme();
+
   return (
     <AppBar 
       elevation={2} 
       sx={{ bgcolor: theme.highlight }}
     >
       <Toolbar variant="dense">
+        <IconButton color="inherit" onClick={onToggleSidebar}>
+          <MenuIcon />
+        </IconButton>
         <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', marginLeft: 2, marginRight: 2 }}>
           <Typography variant="h6" noWrap>
             GESTIÓN DE AMBIENTES
@@ -18,10 +24,7 @@ const BarraSuperior = () => {
         </Box>
         <Box sx={{ display: 'flex' }}>
           <Tooltip title="Notificaciones">
-            <IconButton 
-              color="inherit" 
-            size="large"
-            >
+            <IconButton color="inherit" size="large">
               <NotificationsIcon />
             </IconButton>
           </Tooltip>
@@ -34,6 +37,10 @@ const BarraSuperior = () => {
       </Toolbar>
     </AppBar>
   );
+};
+
+BarraSuperior.propTypes = {
+  onToggleSidebar: PropTypes.func.isRequired
 };
 
 export default BarraSuperior;
