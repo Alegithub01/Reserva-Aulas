45cda1c9d3435dd6eb6348dc801683df605a9c86
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-25 10:06:00 -0400

    Campo "Apellido" agregado

--- frontend/src/PantallaRegistro/PantallaRegistro.jsx
+++ frontend/src/PantallaRegistro/PantallaRegistro.jsx
@@ -1,38 +1,59 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import SplitScreenLayout from "../Components/SplitScreenLayout";
 import TextInput from "../Utils/ValidationTextInput";
 import StyledText from "../StyledText";
 import Button from "../Utils/Button";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import RowPercentage from "../Responsive/RowPercentage";
 
 const RegistroDocente = () => {
   const navegar = useNavigate();
-  const [nombreCompleto, setNombreCompleto] = useState('');
-  const [contrasena, setContrasena] = useState('');
-  const [confirmarContrasena, setConfirmarContrasena] = useState('');
-  const [errores, setErrores] = useState({ nombre: '', contrasena: '', confirmarContrasena: '' });
-  const [camposVacios, setCamposVacios] = useState({ nombre: false, contrasena: false });
+  const [contrasena, setContrasena] = useState("");
+  const [confirmarContrasena, setConfirmarContrasena] = useState("");
   const [dialogoAbierto, setDialogoAbierto] = useState(false);
+  const [nombres, setNombres] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [errores, setErrores] = useState({
+    nombres: "",
+    apellido: "",
+    correo: "",
+    contrasena: "",
+    confirmarContrasena: "",
+  });
+  const [camposVacios, setCamposVacios] = useState({
+    nombres: false,
+    apellido: false,
+    correo: false,
+    contrasena: false,
+  });
 
-  const manejarCambioNombre = (e) => {
+  const manejarCambioNombres = (e) => {
     const valor = e.target.value;
-    const regexNombre = /^[a-zA-Z\s]*$/;
-    if (valor.length <= 50 && regexNombre.test(valor)) {
-      setNombreCompleto(valor);
+    if (valor.length <= 50) {
+      setNombres(valor);
     }
   };
-
+  const manejarCambioCorreo = (e) => {
+    const valor = e.target.value;
+    setCorreo(valor);
+  };
   const manejarCambioContrasena = (e) => {
     const valor = e.target.value;
     if (valor.length <= 20) {
       setContrasena(valor);
     }
   };
-
+  const manejarCambioApellido = (e) => {
+    const valor = e.target.value;
+    if (valor.length <= 50) {
+      setApellido(valor);
+    }
+  };
   const manejarCambioConfirmarContrasena = (e) => {
     const valor = e.target.value;
     if (valor.length <= 20) {
@@ -40,58 +61,94 @@ const RegistroDocente = () => {
     }
   };
 
-  const validarNombreCompleto = () => {
-    const regexNombre = /^[a-zA-Z\s]*$/;
-    let mensajeError = '';
-    if (nombreCompleto.length > 50) {
-      mensajeError = 'El nombre no puede exceder los 50 caracteres.';
-    } else if (!regexNombre.test(nombreCompleto)) {
-      mensajeError = 'El nombre solo puede contener letras y espacios.';
+  const validarCorreo = () => {
+    const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let mensajeError = "";
+    if (!correo) {
+      mensajeError = "Ingrese su correo.";
+    } else if (!regexCorreo.test(correo)) {
+      mensajeError = "Ingrese un correo válido.";
     }
-    setErrores(erroresActuales => ({ ...erroresActuales, nombre: mensajeError }));
+    setErrores((erroresActuales) => ({
+      ...erroresActuales,
+      correo: mensajeError,
+    }));
   };
-
+  const validarNombres = () => {
+    let mensajeError = "";
+    if (!nombres) {
+      mensajeError = "Ingrese su nombre.";
+    }
+    setErrores((erroresActuales) => ({
+      ...erroresActuales,
+      nombres: mensajeError,
+    }));
+  };
   const validarContrasena = () => {
     const regexNumeros = /\d.*\d/;
-    let mensajeError = '';
+    let mensajeError = "";
     if (contrasena.length < 8 || !regexNumeros.test(contrasena)) {
-      mensajeError = 'mínimo 8 caracteres y al menos 2 números.';
+      mensajeError = "mínimo 8 caracteres y al menos 2 números.";
     } else if (contrasena.length > 20) {
-      mensajeError = 'La contraseña no puede exceder los 20 caracteres.';
+      mensajeError = "La contraseña no puede exceder los 20 caracteres.";
     }
-    setErrores(erroresActuales => ({ ...erroresActuales, contrasena: mensajeError }));
+    setErrores((erroresActuales) => ({
+      ...erroresActuales,
+      contrasena: mensajeError,
+    }));
   };
-
-  const validarConfirmacionContrasena = () => {
-    let mensajeError = '';
-    if (confirmarContrasena !== contrasena) {
-      mensajeError = 'Las contraseñas no coinciden.';
+  const validarApellido = () => {
+    let mensajeError = "";
+    if (!apellido) {
+      mensajeError = "Ingrese su apellido.";
     }
-    setErrores(erroresActuales => ({ ...erroresActuales, confirmarContrasena: mensajeError }));
+    setErrores((erroresActuales) => ({
+      ...erroresActuales,
+      apellido: mensajeError,
+    }));
+  };
+  const validarConfirmacionContrasena = () => {
+    let mensajeError = "";
+    if (confirmarContrasena !== contrasena) {
+      mensajeError = "Las contraseñas no coinciden.";
+    }
+    setErrores((erroresActuales) => ({
+      ...erroresActuales,
+      confirmarContrasena: mensajeError,
+    }));
   };
 
   // Validar el formulario completo
   const validarFormulario = () => {
-    validarNombreCompleto();
+    validarNombres();
+    validarApellido();
+    validarCorreo();
     validarContrasena();
     validarConfirmacionContrasena();
 
-    // Verificar campos vacíos
-    const campos = { nombre: nombreCompleto === '', contrasena: contrasena === '' };
+    const campos = {
+      nombres: nombres === "",
+      apellido: apellido === "",
+      correo: correo === "",
+      contrasena: contrasena === "",
+    };
+
     setCamposVacios(campos);
 
-    const formularioEsValido = Object.values(errores).every(error => error === '') && !Object.values(campos).some(empty => empty);
+    const formularioEsValido =
+      Object.values(errores).every((error) => error === "") &&
+      !Object.values(campos).some((empty) => empty);
 
     if (formularioEsValido) {
-      // BACKEND
+      // Proceso de registro
       setDialogoAbierto(true);
     } else {
-      // no
+      // Manejar errores
     }
   };
 
   const contenidoIzquierdo = (
-    <div 
+    <div
       style={{
         display: "flex",
         flexDirection: "column",
@@ -100,9 +157,7 @@ const RegistroDocente = () => {
         height: "20%",
       }}
     >
-      <StyledText boldWhiteText>
-        Reserva de Ambientes
-      </StyledText>
+      <StyledText boldWhiteText>Reserva de Ambientes</StyledText>
     </div>
   );
 
@@ -121,23 +176,51 @@ const RegistroDocente = () => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          margin: "20px 0",
+          margin: "10px 0",
         }}
       >
         <StyledText boldText>Registro de Docente</StyledText>
       </div>
+      <RowPercentage firstChildPercentage={60} gap="10px">
+        <TextInput
+          label="Nombres"
+          isRequired={true}
+          validationMessage={
+            camposVacios.nombres ? "Ingrese su nombre." : errores.nombres
+          }
+          value={nombres}
+          onChange={manejarCambioNombres}
+          onBlur={validarNombres}
+        />
+        <TextInput
+          label="Apellido"
+          isRequired={true}
+          validationMessage={
+            camposVacios.apellido ? "Ingrese su apellido." : errores.apellido
+          }
+          value={apellido}
+          onChange={manejarCambioApellido}
+          onBlur={validarApellido}
+        />
+      </RowPercentage>
       <TextInput
-        label="Nombre Completo"
+        label="Correo"
         isRequired={true}
-        validationMessage={camposVacios.nombre ? 'Ingrese su nombre.' : errores.nombre}
-        value={nombreCompleto}
-        onChange={manejarCambioNombre}
-        onBlur={validarNombreCompleto}
+        validationMessage={
+          camposVacios.correo ? "Ingrese su correo." : errores.correo
+        }
+        value={correo}
+        onChange={manejarCambioCorreo}
+        onBlur={validarCorreo}
       />
       <TextInput
         label="Contraseña"
         isRequired={true}
-        validationMessage={camposVacios.contrasena ? 'Ingrese su contraseña.' : errores.contrasena}
+        validationMessage={
+          camposVacios.contrasena
+            ? "Ingrese su contraseña."
+            : errores.contrasena
+        }
         value={contrasena}
         onChange={manejarCambioContrasena}
         onBlur={validarContrasena}
@@ -151,7 +234,9 @@ const RegistroDocente = () => {
         onBlur={validarConfirmacionContrasena}
         maxLength={20}
       />
-      <Button onClick={validarFormulario} fullWidth={true}>Registro</Button>
+      <Button onClick={validarFormulario} fullWidth={true}>
+        Registro
+      </Button>
 
       <div
         style={{
@@ -160,11 +245,11 @@ const RegistroDocente = () => {
           alignItems: "center",
           flexDirection: "column",
           cursor: "pointer",
-          marginTop: "20px",
+          marginTop: "10px",
         }}
-        onClick={() => navegar('/')}
-        onMouseOver={(e) => e.target.style.color = "#3661EB"}
-        onMouseOut={(e) => e.target.style.color = 'black'}
+        onClick={() => navegar("/")}
+        onMouseOver={(e) => (e.target.style.color = "#3661EB")}
+        onMouseOut={(e) => (e.target.style.color = "black")}
       >
         <StyledText enlaceText> Iniciar sesion </StyledText>
       </div>
@@ -179,14 +264,14 @@ const RegistroDocente = () => {
         onClose={() => setDialogoAbierto(false)}
         aria-describedby="alert-dialog-description"
         sx={{
-          '& .MuiDialog-paper': {
-            borderRadius: '20px',
-          }
+          "& .MuiDialog-paper": {
+            borderRadius: "20px",
+          },
         }}
       >
         <DialogContent
           sx={{
-            borderRadius: '20px',
+            borderRadius: "20px",
           }}
         >
           <DialogContentText id="alert-dialog-description">
@@ -195,14 +280,18 @@ const RegistroDocente = () => {
         </DialogContent>
         <DialogActions
           sx={{
-            justifyContent: 'center',
-            padding: '8px 24px', 
+            justifyContent: "center",
+            padding: "8px 24px",
           }}
         >
-          <Button onClick={() => {
-            setDialogoAbierto(false);
-            navegar('/');
-          }} color="primary" autoFocus>
+          <Button
+            onClick={() => {
+              setDialogoAbierto(false);
+              navegar("/");
+            }}
+            color="primary"
+            autoFocus
+          >
             Aceptar
           </Button>
         </DialogActions>
