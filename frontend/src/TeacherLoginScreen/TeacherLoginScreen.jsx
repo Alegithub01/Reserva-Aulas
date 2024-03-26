c1680ff83450ebb11995865c4a4b51ce46b56620
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-25 22:30:57 -0400

    Cambio de nombre al componente ValidationTextInput

--- frontend/src/TeacherLoginScreen/TeacherLoginScreen.jsx
+++ frontend/src/TeacherLoginScreen/TeacherLoginScreen.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SplitScreenLayout from "../Components/SplitScreenLayout";
-import TextInput from "../Utils/ValidationTextInput";
+import TextInput from "../Utils/CampoValidado";
 import StyledText from "../StyledText";
 import Button from "../Utils/Button";
 
