555d48d1e46faa22c11df1c54b7fb29a7e9c4563
Mateo <mateovaldivia3000@gmail.com>
2024-03-30 02:27:54 -0400

    auth working

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -23,23 +23,7 @@ Route::group ([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::get('/dummy', [AuthController::class, 'dummyGet']);
-
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
-    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
-    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-    Route::post('me', 'App\Http\Controllers\AuthController@me');
-
-    Route::post('register', 'App\Http\Controllers\AuthController@register');
+    Route::get('/register', [AuthController::class, 'register']);
 });
 
-// http://127.0.0.1:8001/api/auth/dummy
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'puta'
-], function ($router) {
-    Route::get('/hello', [HelloController::class, 'hello']);
-});
-
-// GET http://127.0.0.1:8001/api/puta/hello
