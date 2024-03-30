9c7d7d53653a55d9d89e02b60dcdaa7a40366285
Mateo <mateovaldivia3000@gmail.com>
2024-03-30 01:43:54 -0400

    estamos

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group ([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::get('/dummy', 'App\Http\Controllers\AuthController@dummyGet');
+
+    // Route::post('login', 'App\Http\Controllers\AuthController@login');
+    // Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    // Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    // Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+    // Route::post('register', 'App\Http\Controllers\AuthController@register');
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'puta'
+], function ($router) {
+    Route::get('/hello', 'App\Http\Controllers\HelloController@hello');
+});
+
+// GET http://127.0.0.1:8001/api/puta/hello
