7ac0b3e44691a6ab9f04e1c17edc295fbb61ed0a
Mateo <mateovaldivia3000@gmail.com>
2024-04-07 02:31:26 -0400

    tipso de datos iguales

--- backend/database/migrations/2024_04_07_061908_change_id_column_type_in_roles_table.php
+++ backend/database/migrations/2024_04_07_061908_change_id_column_type_in_roles_table.php
@@ -7,11 +7,15 @@ class ChangeIdColumnTypeInRolesTable extends Migration
     public function up()
     {
         DB::statement('ALTER TABLE rols MODIFY id INT UNSIGNED');
+        DB::statement('ALTER TABLE users MODIFY id INT UNSIGNED');
+        DB::statement('ALTER TABLE users MODIFY idRol INT UNSIGNED');
     }
 
     public function down()
     {
         DB::statement('ALTER TABLE rols MODIFY id INT');
+        DB::statement('ALTER TABLE users MODIFY id INT');
+        DB::statement('ALTER TABLE users MODIFY idRol INT');
     }
 }
 
