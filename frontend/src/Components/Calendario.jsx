a8c65b6cdbc085984807c8545ac76113f1b145a1
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-04-13 13:15:43 -0400

    Tooltip en celda seleccionada

--- frontend/src/Components/Calendario.jsx
+++ frontend/src/Components/Calendario.jsx
@@ -1,87 +1,95 @@
-  import React, { useState } from 'react';
-  import { startOfWeek, addDays, format } from 'date-fns';  
+import { useState } from 'react';
+import { startOfWeek, addDays, format } from 'date-fns';
+import styled from 'styled-components';
+import Tooltip from '@mui/material/Tooltip';
 
-  const calendarStyles = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
+const CalendarStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const RowStyles = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const CellStyles = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  border: 1px solid #EAEAEA;
+  min-height: 35px;
+  &:hover {
+    border: 1px solid #3661EB;
+  }
+`;
+
+const HeaderStyles = styled(CellStyles)`
+  color: #55555D;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const Calendar = ({ schedule }) => {
+  const [currentWeekStart, setCurrentWeekStart] = useState(startOfWeek(new Date(), { weekStartsOn: 1 }));
+  const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  const hours = ['06:45', '08:15', '09:45', '11:15', '12:45', '14:15', '15:45', '17:15', '18:45', '20:15', '21:45'];
+
+  const goToPreviousWeek = () => {
+    setCurrentWeekStart(addDays(currentWeekStart, -7));
   };
 
-  const rowStyles = {
-    display: 'flex',
-    width: '100%',
+  const goToNextWeek = () => {
+    setCurrentWeekStart(addDays(currentWeekStart, 7));
   };
 
-  const cellStyles = {
-    display: 'flex',     
-    alignItems: 'center',    
-    justifyContent: 'center',
-    flex: 1,
-    border: '1px solid #ddd',
-    minHeight: '40px',
-  };
-
-  const occupiedCellStyles = {
-    ...cellStyles,
-    backgroundColor: '#f8d7da',
-  };
-
-  const Calendar = ({ schedule }) => {
-    const [currentWeekStart, setCurrentWeekStart] = useState(startOfWeek(new Date(), { weekStartsOn: 1 }));
-
-    const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
-    const hours = ['06:45', '08:15', '09:45', '11:15', '12:45', '14:15', '15:45', '17:15', '18:45', '20:15', '21:45'];
-
-    const goToPreviousWeek = () => {
-      setCurrentWeekStart(addDays(currentWeekStart, -7));
-    };
-
-    const goToNextWeek = () => {
-      setCurrentWeekStart(addDays(currentWeekStart, 7));
-    };
-
-    return (
-      <div style={calendarStyles}>
-        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-          <button onClick={goToPreviousWeek}>{"<"}</button>
-          <button onClick={goToNextWeek}>{">"}</button>
-        </div>
-        <div style={rowStyles}>
-          <div style={{ ...cellStyles, fontWeight: 'bold' }}>Hora</div> {/* Encabezado de la columna de horas */}
-          {days.map((day, index) => {
-            const date = addDays(currentWeekStart, index);
-            return (
-              <div key={day} style={{ ...cellStyles, fontWeight: 'bold', textAlign:'center'}}>
-                {`${day}`}
-                <br style={{ lineHeight: '0.8em' }} />
-                {`${format(date, 'dd')}`}
-              </div>
-            );
-          })}
-        </div>
-        {hours.map(hour => (
-        <div key={hour} style={rowStyles}>
-          <div style={{ ...cellStyles, fontWeight: 'bold', textAlign:'center'}}>
+  return (
+    <CalendarStyles>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+        <button onClick={goToPreviousWeek}>{"<"}</button>
+        <button onClick={goToNextWeek}>{">"}</button>
+      </div>
+      <RowStyles>
+        <HeaderStyles>Hora</HeaderStyles>
+        {days.map((day, index) => {
+          const date = addDays(currentWeekStart, index);
+          return (
+            <HeaderStyles key={day}>
+              {`${day}`}
+              <br style={{ lineHeight: '0.8em' }} />
+              {`${format(date, 'dd')}`}
+            </HeaderStyles>
+          );
+        })}
+      </RowStyles>
+      {hours.map(hour => (
+        <RowStyles key={hour}>
+          <HeaderStyles>
             {hour}
-          </div>
+          </HeaderStyles>
           {days.map((day, index) => {
             const dayDate = format(addDays(currentWeekStart, index), 'yyyy-MM-dd');
-            // Verifica si la hora está incluida en el array para ese día
             const isReserved = schedule[dayDate] && schedule[dayDate].includes(hour);
+            const occupiedCellStyles = {
+              backgroundColor: isReserved ? '#F1F5FF' : 'transparent',
+              color:'#55555D',
+            };
             return (
-              <div
-                key={`${day}-${hour}`}
-                style={isReserved ? occupiedCellStyles : cellStyles}
-              >
-                {isReserved ? 'Ocupado' : ''}
-              </div>
+              <Tooltip key={`${day}-${hour}`} title={`${day}, ${format(addDays(currentWeekStart, index), 'dd/MM/yyyy')} - ${hour}`}>
+                <CellStyles style={occupiedCellStyles}>
+                  {isReserved ? 'Ocupado' : ''}
+                </CellStyles>
+              </Tooltip>
             );
           })}
-        </div>
+        </RowStyles>
       ))}
-      </div>
-    );
-  };
-  
-  export default Calendar;
+    </CalendarStyles>
+  );
+};
+
+export default Calendar;
