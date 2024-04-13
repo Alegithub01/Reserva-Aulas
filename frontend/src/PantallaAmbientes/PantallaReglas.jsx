6b1ecdbec876008fec4de2b222c0fc9bd37388ef
judithmarg <judithmargaritapacollalli@gmail.com>
2024-04-13 01:48:34 -0400

    campo textarea guarda valor

--- frontend/src/PantallaAmbientes/PantallaReglas.jsx
+++ frontend/src/PantallaAmbientes/PantallaReglas.jsx
@@ -1,10 +1,62 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
 import Card from "../Utils/Card";
 import StyledText from "../StyledText";
 import { useTheme } from '../Contexts/ThemeContext';
 import ButtonEstilizado from "../Utils/Button";
 
+const TextAreaP = ({ onChange, value }) => {
+  const [valor, setValor] = useState(value);
+
+  useEffect(() => {
+    onChange && onChange(valor);
+  }, [valor, onChange]);
+
+  const cambio = (e) => {
+    if (e && e.target) {
+    if (onChange) {
+      onChange(valor)
+    }
+    if(valor.length < 219){
+      setValor(e.target.value);
+    }
+  }
+  };
+  return (
+    <textarea
+      style={{
+        width: '100%',
+        height: '40%',
+        borderRadius: '20px',
+        formSizing: 'border-box',
+        padding: '15px',
+        fontFamily: 'Arial',
+        fontSize: '16px',
+      }}
+      onChange={cambio}
+      value={value}
+    />
+  );
+};
+TextAreaP.propTypes = {
+  onChange: PropTypes.func,
+  value: PropTypes.string,
+};
+
+
 const AdminHomeModule3 = () => {
   const { theme } = useTheme();
+  const [reglas, setReglas] = useState('');
+
+  const guardarBaseDatos = () => {
+    console.log(reglas);
+    //aca guardar en la base de datos reglas, considerando el estado publicado false
+  }
+
+  const publicar = () => {
+    //aca actualizar el estado de publicado a true
+  }
+
   const defaultStyle = {
     outerContainer: {
       display: 'flex',
@@ -21,21 +73,11 @@ const AdminHomeModule3 = () => {
       minHeight: '450px',
     },
     buttonsField: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: '20px',
-        marginTop: '20px',
-    },
-    textfield:{
-        width: '100%',
-        height: '40%',
-        borderRadius: '20px',
-        formSizing: 'border-box',
-        padding: '15px',
-        fontFamily: 'Arial',
-        fontSize: '16px',
-        border: `2px solid ${theme.highlight}`
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      gap: '20px',
+      marginTop: '20px',
     },
   };
   return (
@@ -56,27 +98,30 @@ const AdminHomeModule3 = () => {
               height: "100%",
               display: "flex",
               flexDirection: "column",
-            
+
             }}
           >
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  height: '30%',
-                }}
-              >
-                <StyledText boldText >Reglas de Reserva</StyledText>
-                </div>
-                <textarea style={defaultStyle.textfield}/>
-                <div style={defaultStyle.buttonsField}>
-                    <ButtonEstilizado fullWidth={true} onClick={() => {}} >Guardar</ButtonEstilizado>
-                    <ButtonEstilizado fullWidth={true} onClick={() => {}} >Publicar</ButtonEstilizado>
-                </div>
-              
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '30%',
+              }}
+            >
+              <StyledText boldText >Reglas de Reserva</StyledText>
             </div>
-       
+            <TextAreaP
+              onChange={setReglas }
+              value={reglas}
+            />
+            <div style={defaultStyle.buttonsField}>
+              <ButtonEstilizado fullWidth={true} onClick={guardarBaseDatos} >Guardar</ButtonEstilizado>
+              <ButtonEstilizado fullWidth={true} onClick={publicar} >Publicar</ButtonEstilizado>
+            </div>
+
+          </div>
+
         </Card>
       </div >
     </div >
