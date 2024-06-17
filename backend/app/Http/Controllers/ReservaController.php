4c9a039301ce6d019ed8dd1cc814be2128a51ae4
joseCaceresGit <jacaceresc12@gmail.com>
2024-06-17 19:36:54 -0400

    .

--- backend/app/Http/Controllers/ReservaController.php
+++ backend/app/Http/Controllers/ReservaController.php
@@ -42,25 +42,32 @@ class ReservaController extends Controller
 
     public function asignarGrupal(Request $request)
     {
+        // Buscar la solicitud grupal por su ID
         $solicitud = SolicitudGrupal::findOrFail($request->id);
 
+        // Buscar si ya existe una reserva para esta solicitud grupal
         $reservaExistente = Reserva::where('solicitable_id', $request->id)
                                 ->where('solicitable_type', SolicitudGrupal::class)
                                 ->first();
 
         if ($reservaExistente) {
+            // Si la reserva ya existe, actualizar la asignación de aulas
             $reservaExistente->aulas = json_encode($request->aulas);
             $reservaExistente->save();
         } else {
+            // Si la reserva no existe, crear una nueva reserva
             $reserva = new Reserva();
             $reserva->solicitable_id = $request->id;
-            $reserva->solicitable_type = Solicitud::class;
+            $reserva->solicitable_type = SolicitudGrupal::class;
             $reserva->aulas = json_encode($request->aulas);
             $reserva->save();
         }
 
+        // Actualizar el estado de la solicitud grupal
         $solicitud->estado = 'Asignada';
         $solicitud->save();
+
+        // Devolver una respuesta exitosa
         return response(['message' => 'Aulas asignadas a las reservas'], 200);
     }
 }
